421c770f282343cb0ad7e40ef9bc9742ce314752
OishiMisaki <oishi.misaki@m.alhinc.jp>
2017-05-11 18:43:19 +0900

    1st

---
+++ UserComment.java
@@ -0,0 +1,61 @@
+package bulletinBoard.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class UserComment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int comment_id;
+	private int user_id;
+	private int message_id;
+	private String login_id;
+	private String name;
+	private String text;
+	private Date insert_time;
+
+	public int getMessage_id() {
+		return message_id;
+	}
+	public void setMessage_id(int message_id) {
+		this.message_id = message_id;
+	}
+
+	public int getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(int comment_id) {
+		this.comment_id = comment_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getInsert_time() {
+		return insert_time;
+	}
+	public void setInsert_time(Date insert_time) {
+		this.insert_time = insert_time;
+	}
+
+}
