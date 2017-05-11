0ff111e63481a6e0b487630826aa1eb022390766
Oishi-Misaki <oishi.misaki@m.alhinc.jp>
2017-05-11 18:50:55 +0900

    Add files via upload

---
+++ CommentService.java
@@ -0,0 +1,60 @@
+package bulletinBoard.service;
+
+import static bulletinBoard.utils.CloseableUtil.*;
+import static bulletinBoard.utils.DBUtil.*;
+
+import java.sql.Connection;
+import java.util.List;
+
+import bulletinBoard.beans.Comment;
+import bulletinBoard.beans.UserComment;
+import bulletinBoard.dao.CommentDao;
+import bulletinBoard.dao.UserCommentDao;
+
+public class CommentService {
+	public void register(Comment comment) {
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			CommentDao commentDao = new CommentDao();
+			commentDao.insert(connection, comment);
+
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
+	private static final int LIMIT_NUM = 1000;
+
+	public List<UserComment> getUserComment() {
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			UserCommentDao userCommentDao = new UserCommentDao();
+			List<UserComment> ret = userCommentDao.getUserComment(connection, LIMIT_NUM);
+
+			commit(connection);
+			return ret;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
+
+
+}
